bfb4118bce87a204c36fd7e2320582a053651c66
mudassir060 <mudassirmukhtar4@gmail.com>
2020-11-07 16:46:06 +0500

    first commit

---
+++ src/components/Emojis.jsx
@@ -0,0 +1,22 @@
+// import React from "react";
+// import { Editor } from "@tinymce/tinymce-react";
+// import "./styles.css";
+
+// export default function Emojis() {
+//   return (
+//     <Editor
+//       apiKey="qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc"
+//       init={{
+//         plugins: "emoticons",
+//         toolbar: "emoticons",
+//         toolbar_location: "bottom",
+//         menubar: false,
+//         statusbar: false,
+//         height: 450
+//       }}
+//       initialValue="<p></p>"
+      
+//     />
+//   );
+// }
+
